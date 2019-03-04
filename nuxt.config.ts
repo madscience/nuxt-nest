4535e3863ec4a4acd8378f9ef26da1006120f6ee
Simon Garner <simon.garner@madscience.co.nz>
2019-03-05 12:00:07 +1300

    Set nuxt config.dev mode based on NODE_ENV environment variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import NuxtConfiguration from '@nuxt/config';
 
 const config: NuxtConfiguration = {
   srcDir: 'src/frontend/',
+  dev: (process.env.NODE_ENV !== 'production'),
   plugins: ['~/plugins/hello'],
 };
 
