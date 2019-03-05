63f2545bb4e8ff51c520ecce359ecd298e14c1d4
Simon Garner <simon.garner@madscience.co.nz>
2019-03-05 16:16:34 +1300

    Make it clear which app is saying hello

--- src/frontend/components/HelloWorld.vue
+++ src/frontend/components/HelloWorld.vue
@@ -9,6 +9,6 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
-  message: string = 'Hello world !';
+  message: string = 'Hello from Nuxt!';
 }
 </script>
